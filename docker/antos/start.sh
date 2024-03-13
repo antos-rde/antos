860b61b04e24a4b8ae4b69273de5d1b88cdf3888
DanyLE <xsang.le@gmail.com>
2024-03-13 22:51:01 +0100

    fix: proper way to handle signal + wait for process

--- docker/antos/start.sh
+++ docker/antos/start.sh
@@ -15,7 +15,28 @@ unset ANTOS_PASSWORD
 syslogd -O /tmp/message
 
 antd /opt/www/etc/antd-config.ini &
+pid=$!
 sleep 2
 runner /opt/www/etc/runner.ini &
+#pid_runner=$!
 
-cat
+for signal in TERM USR1 HUP INT; do
+  # shellcheck disable=SC2064
+  trap "echo SIGNAL: $signal; kill -s $signal $pid" $signal
+done
+
+# USR2 converted to WINCH
+# shellcheck disable=SC2064
+trap "kill -s WINCH $pid" USR2
+
+status=999
+while true; do
+  if [ "${status}" -le 128 ]; then
+    # Status codes larger than 128 indicates a trapped signal terminated the wait command (128 + SIGNAL).
+    # In any other case we can stop the loop. 
+    break
+  fi
+  wait $pid
+  status=$?
+  echo exit status: $status
+done
